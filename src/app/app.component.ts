2b83dcecd76c1fa538194076d92eefae4cc632e6
MonsieurHao <totocosta29@hotmail.fr>
2023-09-22 18:10:56 +0200

    Addition of first buttons and dropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularTraining';
+onUpdateList() {
+console.log("Actualise le tableau");
+}
+onReinitCritere() {
+  console.log("Réinitialise les critères");
+}
+  rowData: any[] = [
+    {num_cotisant: 8, début_période: '29/09/2022', fin_période: '29/10/2023'},
+    {num_cotisant: 752, début_période: '15/02/2022', fin_période: '15/12/2023'},
+    {num_cotisant: 68, début_période: '03/04/2022', fin_période: '03/09/2023'}
+  ];
+  colDefs: ColDef[] = [
+    {field: 'Numéro de cotisant', sortable: true, filter: true},
+    {field: 'Début période', sortable: true, filter: true},
+    {field: 'Fin période', sortable: true, filter: true},
+    {field: 'type de compte', sortable: true, filter: true},
+    {field: 'nature de l activité', sortable: true, filter: true},
+    {field: 'catégorie de cotisant', sortable: true, filter: true},
+    {field: 'état de l image', sortable: true, filter: true}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UtilisateursComponent } from './Composants/utilisateurs/utilisateurs.component';
+import { AgGridModule } from 'ag-grid-angular';
+//import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilisateursComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
